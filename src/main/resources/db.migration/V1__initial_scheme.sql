0cb0c4f50881b44055dbe69718b295836c22ee5b
Cristian <cchoto@prosupply.ec>
2024-10-05 00:46:48 -0500

    [CS-003] add configuration cicd and docker files

--- src/main/resources/db.migration/V1__initial_scheme.sql
+++ src/main/resources/db.migration/V1__initial_scheme.sql
@@ -1,15 +1,15 @@
 CREATE TABLE customers(
-    ctr_id Long PRIMARY KEY ,
-    ctr_dni String NOT NULL UNIQUE,
-    ctr_f_name String NOT NULL ,
-    crt_l_name String NOT NULL ,
-    ctr_genre String NOT NULL
+    ctr_id SERIAL PRIMARY KEY ,
+    ctr_dni VARCHAR NOT NULL UNIQUE,
+    ctr_f_name VARCHAR NOT NULL ,
+    crt_l_name VARCHAR NOT NULL ,
+    ctr_genre VARCHAR NOT NULL
 );
 
 CREATE TABLE customer_audit(
-    id Long PRIMARY KEY,
-    customer_id Long NOT NULL,
-    user_name String NOT NULL,
-    action String NOT NULL,
-    local_date_time DATE
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    user_name VARCHAR NOT NULL,
+    action VARCHAR NOT NULL,
+    local_date_time TIMESTAMP
 );
